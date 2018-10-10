adae72964374db315ac826fff117ea8a687bbd27
Michal Moskala <mms@trifork.com>
2018-10-10 09:54:08 +0300

    Added LottoAPI iOS framework

---
+++ LottoAPI/LottoAPI.h
@@ -0,0 +1,19 @@
+//
+//  LottoAPI.h
+//  LottoAPI
+//
+//  Created by Michal Moskala on 10/10/2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LottoAPI.
+FOUNDATION_EXPORT double LottoAPIVersionNumber;
+
+//! Project version string for LottoAPI.
+FOUNDATION_EXPORT const unsigned char LottoAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LottoAPI/PublicHeader.h>
+
+
