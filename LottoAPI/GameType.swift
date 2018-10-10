0ffe2c26603f4a56fcd4210a9e9fbca0f6fbac08
Michał Moskała <moskala.michal@gmail.com>
2018-10-10 10:00:27 +0300

    Added GameType enum

---
+++ LottoAPI/GameType.swift
@@ -0,0 +1,15 @@
+//
+//  GameType.swift
+//  LottoAPI
+//
+//  Created by Michal Moskala on 10/10/2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+public enum GameType {
+    case lotto
+    case plus
+    case mini
+}
