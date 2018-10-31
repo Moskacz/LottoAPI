21beabefda9192cccda0348e8b707eb1bcddef68
Michał Moskała <moskala.michal@gmail.com>
2018-10-31 09:59:44 +0100

    new results in LotteriesResults

--- LottoAPI/LotteryResults/LotteriesResults.swift
+++ LottoAPI/LotteryResults/LotteriesResults.swift
@@ -13,8 +13,13 @@ public protocol LotteriesResults {
     var superszansa: [LotteryResult]? { get }
     var lottoPlus: LotteryResult? { get }
     var mini: LotteryResult? { get }
+    var joker: LotteryResultJoker? { get }
     var kaskada1: LotteryResult? { get }
     var kaskada2: LotteryResult? { get }
+    var mm1: LotteryResultPlus? { get }
+    var mm2: LotteryResultPlus? { get }
+    var extrapensja: LotteryResultExtra? { get }
     var ss1: LotteryResult? { get }
     var ss2: LotteryResult? { get }
+    var ej: LotteryResultExtra? { get }
 }
--- LottoAPI/LotteryResults/LotteriesResultsImpl.swift
+++ LottoAPI/LotteryResults/LotteriesResultsImpl.swift
@@ -10,23 +10,35 @@ import Foundation
 
 struct LotteriesResultsImpl: LotteriesResults, Decodable {
     
-    private var _lotto: LotteryResultImpl?
+    private let _lotto: LotteryResultImpl?
     var lotto: LotteryResult? { return _lotto }
     
-    private var _superszansa: [LotteryResultImpl]?
+    private let _superszansa: [LotteryResultImpl]?
     var superszansa: [LotteryResult]? { return _superszansa }
     
-    private var _lottoPlus: LotteryResultImpl?
+    private let _lottoPlus: LotteryResultImpl?
     var lottoPlus: LotteryResult? { return _lottoPlus }
     
-    private var _mini: LotteryResultImpl?
+    private let _mini: LotteryResultImpl?
     var mini: LotteryResult? { return _mini }
     
-    private var _kaskada1: LotteryResultImpl?
+    private let _joker: LotteryResultJokerImpl?
+    var joker: LotteryResultJoker? { return _joker }
+    
+    private let _kaskada1: LotteryResultImpl?
     var kaskada1: LotteryResult? { return _kaskada1 }
     
-    private var _kaskada2: LotteryResultImpl?
+    private let _kaskada2: LotteryResultImpl?
     var kaskada2: LotteryResult? { return _kaskada2 }
+    
+    private let _mm1: LotteryResultPlusImpl?
+    var mm1: LotteryResultPlus? { return _mm1 }
+    
+    private let _mm2: LotteryResultPlusImpl?
+    var mm2: LotteryResultPlus? { return _mm2 }
+    
+    private let _extrapensja: LotteryResultExtraImpl?
+    var extrapensja: LotteryResultExtra? { return _extrapensja }
     
     private var _ss1: LotteryResultImpl?
     var ss1: LotteryResult? { return _ss1 }
@@ -34,14 +46,22 @@ struct LotteriesResultsImpl: LotteriesResults, Decodable {
     private var _ss2: LotteryResultImpl?
     var ss2: LotteryResult? { return _ss2 }
     
+    private let _ej: LotteryResultExtraImpl?
+    var ej: LotteryResultExtra? { return _ej }
+    
     enum CodingKeys: String, CodingKey {
         case _lotto = "Lotto"
         case _superszansa = "Superszansa"
         case _lottoPlus = "LottoPlus"
         case _mini = "Mini"
+        case _joker = "Joker"
         case _kaskada1 = "Kaskada1"
         case _kaskada2 = "Kaskada2"
+        case _mm1 = "MM1"
+        case _mm2 = "MM2"
+        case _extrapensja = "EkstraPensja"
         case _ss1 = "SS1"
         case _ss2 = "SS2"
+        case _ej = "EJ"
     }
 }
